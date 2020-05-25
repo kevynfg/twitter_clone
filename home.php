45dbbb86d800335aea4e7ccf0abb5fd74d72acbe
kevyn <kevynredlotus@gmail.com>
2020-05-24 21:34:52 -0300

    twitter image experiment

--- home.php
+++ home.php
@@ -4,42 +4,7 @@
 	if(!$_SESSION['usuario']){
 		header('Location: index.php?erro=1');
 	}
-	/*
-	require_once('db.class.php');
 
-	$objDb = new db();
-	$link = $objDb->conecta_mysql();
-
-	$id_usuario = $_SESSION['id_usuario'];
-
-	//qtde de tweets
-	$sql = " SELECT COUNT(*) AS qtde_tweets FROM tweet WHERE id_usuario = $id_usuario ";
-	$resultado_id = mysqli_query($link, $sql);
-
-	$qtde_tweets = 0;
-
-	if($resultado_id){
-		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
-		$qtde_tweets = $registro['qtde_tweets'];
-		
-	} else {
-		echo 'Erro no banco de dados';
-	}
-
-	//qtde de seguidores
-	$sql = " SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores WHERE seguindo_id_usuario = $id_usuario ";
-	$resultado_id = mysqli_query($link, $sql);
-
-	$qtde_seguidores = 0;
-
-	if($resultado_id){
-		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
-		$qtde_seguidores = $registro['qtde_seguidores'];
-		
-	} else {
-		echo 'Erro no banco de dados';
-	}
-	*/
 ?>
 
 <!DOCTYPE HTML>
